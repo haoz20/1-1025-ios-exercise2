d6f866a5e65fb517c08c289646195c2126608bb8
haoz20 <zhaonianhao2005@gmail.com>
2025-06-14 22:15:45 +0700

    Completed

--- Exercise2/ViewController.swift
+++ Exercise2/ViewController.swift
@@ -9,8 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var dispalyUILabel: UILabel!
+    @IBOutlet var displayText: UILabel!
     @IBOutlet var displayImage: UIImageView!
+    @IBOutlet var button: UIButton!
     
 
     override func viewDidLoad() {
@@ -19,19 +20,28 @@ class ViewController: UIViewController {
     }
     
     @IBAction func loadGiraffe() {
+        displayImage.image = #imageLiteral(resourceName: "Image")
+        displayText.text = "A giraffe has yellow color"
+        displayText.textColor = UIColor.systemYellow
         
     }
     
     @IBAction func loadPig() {
-        
+        displayImage.image = #imageLiteral(resourceName: "Image 2")
+        displayText.text = "A pig has pink color"
+        displayText.textColor = UIColor(red: 248/255, green: 151/255, blue: 214/255, alpha: 1.0)
     }
     
     @IBAction func loadCrocodile() {
-        
+        displayImage.image = #imageLiteral(resourceName: "Image 3")
+        displayText.text = "A crocodile has green color"
+        displayText.textColor = UIColor(red: 0/255, green: 183/255, blue: 63/255, alpha: 1.0)
     }
     
     @IBAction func loadShark() {
-        
+        displayImage.image = #imageLiteral(resourceName: "Image 1")
+        displayText.text = "A shark has blue color"
+        displayText.textColor = UIColor(red: 115/255, green: 203/255, blue: 219/255, alpha: 1.0)
     }
     
 
