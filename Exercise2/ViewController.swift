c314fa0e0d10ea046dbbbf85337655d510f831e2
haoz20 <zhaonianhao2005@gmail.com>
2025-06-14 20:37:09 +0700

    Initial Commit

---
+++ Exercise2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exercise2
+//
+//  Created by Swan Htet Aung on 14/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
