de9df520646aee1cc1300997734b011d3a8201b7
haoz20 <zhaonianhao2005@gmail.com>
2025-06-14 21:24:35 +0700

    Initialize the codes

--- Exercise2/ViewController.swift
+++ Exercise2/ViewController.swift
@@ -8,11 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var dispalyUILabel: UILabel!
+    @IBOutlet var displayImage: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func loadGiraffe() {
+        
+    }
+    
+    @IBAction func loadPig() {
+        
+    }
+    
+    @IBAction func loadCrocodile() {
+        
+    }
+    
+    @IBAction func loadShark() {
+        
+    }
+    
 
 
 }
